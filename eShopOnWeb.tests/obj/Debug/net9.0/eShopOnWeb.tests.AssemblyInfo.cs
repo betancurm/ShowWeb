482fa1d2e589a69ffa8d9ce26468e22339e7f985
Juan Manuel Betancur <juanb9410@gmail.com>
2025-03-24 20:36:14 -0500

    Carga en repositorio

--- eShopOnWeb.tests/obj/Debug/net9.0/eShopOnWeb.tests.AssemblyInfo.cs
+++ eShopOnWeb.tests/obj/Debug/net9.0/eShopOnWeb.tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eShopOnWeb.tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1694221fe48bb69e1418649fa1b042c772eec37a")]
 [assembly: System.Reflection.AssemblyProductAttribute("eShopOnWeb.tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eShopOnWeb.tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
